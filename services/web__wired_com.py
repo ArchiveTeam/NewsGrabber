78e073a59dab51875e29a6492ef4a8352a66fee8
Start <PressStartandSelect@users.noreply.github.com>
2015-12-20 14:06:20 -0700

    fix spacing

--- services/web__wired_com.py
+++ services/web__wired_com.py
@@ -3,8 +3,8 @@ version = 20151220.01
 
 urls = ['http://www.wired.com/feed/',
         'http://www.wired.com',
-		'http://feeds.cnevids.com/brand/wired.mrss',
-		'http://video.wired.com']
+        'http://feeds.cnevids.com/brand/wired.mrss',
+        'http://video.wired.com']
 regex = [r'^https?:\/\/[^\/]*wired\.com\/',
 	    r'^https?:\/\/[^\/]*cnevids\.com\/']	
 videoregex = [r'/watch/']
