15884c09a73fbbbbca0a444a0e7ad31dfc20bdba
Start <PressStartandSelect@users.noreply.github.com>
2015-12-20 13:17:23 -0700

    add the beaverton

---
+++ services/web__thebeaverton_com.py
@@ -0,0 +1,10 @@
+refresh = 8
+version = 20151220.01
+
+urls = ['http://www.thebeaverton.com/?format=feed&type=rss',
+        'http://www.thebeaverton.com']
+regex = [r'^https?:\/\/[^\/]*thebeaverton\.com\/']
+videoregex = []
+liveregex = []
+
+
