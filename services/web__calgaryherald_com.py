18e27297f81f2933f004d51971ac75af4328805f
Start <PressStartandSelect@users.noreply.github.com>
2015-12-20 13:31:09 -0700

    add calgary herald

---
+++ services/web__calgaryherald_com.py
@@ -0,0 +1,10 @@
+refresh = 8
+version = 20151220.01
+
+urls = ['http://calgaryherald.com/feed',
+        'http://calgaryherald.com']
+regex = [r'^https?:\/\/[^\/]*calgaryherald\.com\/']
+videoregex = [r'^https?:\/\/[^\/]*kaltura\.com\/']
+liveregex = []
+
+
