bd262c0c47659d9bd3b186790e73666546141a79
Start <PressStartandSelect@users.noreply.github.com>
2015-12-20 13:28:40 -0700

    add edmonton journal

---
+++ services/web__edmontonjournal_com.py
@@ -0,0 +1,10 @@
+refresh = 4
+version = 20151220.01
+
+urls = ['http://edmontonjournal.com/feed',
+        'http://edmontonjournal.com']
+regex = [r'^https?:\/\/[^\/]*edmontonjournal\.com\/']
+videoregex = [r'^https?:\/\/[^\/]*kaltura\.com\/']
+liveregex = []
+
+
