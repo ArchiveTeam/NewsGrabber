a513bf4031e63bae0c693aa78ab9cb81e3272f27
PressStartandSelect <attackedbyglitch@gmail.com>
2015-12-20 14:05:21 -0700

    add wired and fix the onion

--- services/web__theonion_com.py
+++ services/web__theonion_com.py
@@ -1,11 +1,11 @@
 refresh = 4
-version = 20151220.02
+version = 20151220.03
 
 urls = ['http://www.theonion.com/feeds/rss',
         'http://www.theonion.com',
         'http://www.onionstudios.com/feeds/rss',
         'http://www.onionstudios.com']
-regex = [r'^https?:\/\/[^\/]*theonion\.com\/']
+regex = [r'^https?:\/\/[^\/]*theonion\.com\/',
 	    r'^https?:\/\/[^\/]*onionstudios\.com\/']
 videoregex = [r'/video/']
 liveregex = []
---
+++ services/web__wired_com.py
@@ -0,0 +1,13 @@
+refresh = 4
+version = 20151220.01
+
+urls = ['http://www.wired.com/feed/',
+        'http://www.wired.com',
+		'http://feeds.cnevids.com/brand/wired.mrss',
+		'http://video.wired.com']
+regex = [r'^https?:\/\/[^\/]*wired\.com\/',
+	    r'^https?:\/\/[^\/]*cnevids\.com\/']	
+videoregex = [r'/watch/']
+liveregex = []
+
+
