3e9a7c9293c1f3c29cbf84b5a16808dd3e4d895a
Start <PressStartandSelect@users.noreply.github.com>
2015-12-20 13:11:26 -0700

    add the onion

---
+++ services/web_theonion_com.py
@@ -0,0 +1,12 @@
+refresh = 4
+version = 20151220.01
+
+urls = ['http://www.theonion.com/feeds/rss',
+        'http://www.theonion.com',
+        'http://www.onionstudios.com/feeds/rss',
+        'http://www.onionstudios.com']
+regex = [r'^https?:\/\/[^\/]*theonion\.com\/']
+videoregex = [r'/video/']
+liveregex = []
+
+
