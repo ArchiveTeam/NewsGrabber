b437907c328a43c36fdddeef14751ee868ca487f
PressStartandSelect <attackedbyglitch@gmail.com>
2015-12-21 13:45:16 -0700

    add citynews.ca

---
+++ services/web__citynews_ca.py
@@ -0,0 +1,10 @@
+refresh = 3
+version = 20151221.01
+
+urls = ['http://www.citynews.ca/feed/']	
+regex = [r'^https?:\/\/[^\/]*citynews\.ca\/']	
+videoregex = [r'\?show_id=',
+	    r'\/video-']
+liveregex = []
+
+
