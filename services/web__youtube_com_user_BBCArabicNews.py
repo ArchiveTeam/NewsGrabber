94d78a24b2854a469d1aa89b04d7126942528f51
PressStartandSelect <attackedbyglitch@gmail.com>
2015-12-20 21:19:09 -0700

    add cbc and many more youtube channels

---
+++ services/web__youtube_com_user_BBCArabicNews.py
@@ -0,0 +1,8 @@
+refresh = 4
+version = 20151220.01
+
+urls = ['https://www.youtube.com/user/BBCArabicNews/videos']
+regex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
+videoregex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
+liveregex = []
+
---
+++ services/web__youtube_com_user_pmocpm.py
@@ -0,0 +1,8 @@
+refresh = 11
+version = 20151220.01
+
+urls = ['https://www.youtube.com/user/pmocpm/videos']
+regex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
+videoregex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
+liveregex = []
+
