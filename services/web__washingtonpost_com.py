85b1e4dff8632773b56dc3654b8f7dbe3ab45d75
PressStartandSelect <attackedbyglitch@gmail.com>
2015-12-21 11:30:53 -0700

    add washington post

---
+++ services/web__washingtonpost_com.py
@@ -0,0 +1,21 @@
+refresh = 3
+version = 20151221.01
+
+urls = ['http://feeds.washingtonpost.com/rss/politics',
+        'http://feeds.washingtonpost.com/rss/opinions',
+        'http://feeds.washingtonpost.com/rss/sports',
+        'http://feeds.washingtonpost.com/rss/national',
+        'http://feeds.washingtonpost.com/rss/world',
+        'http://feeds.washingtonpost.com/rss/local',
+        'http://feeds.washingtonpost.com/rss/business',
+        'http://feeds.washingtonpost.com/rss/entertainment',
+        'http://feeds.washingtonpost.com/rss/lifestyle',
+        'http://feeds.washingtonpost.com/rss/rss_where-we-live',
+        'https://www.washingtonpost.com/posttv/',
+        'https://www.washingtonpost.com/photography/']
+regex = [r'^https?:\/\/[^\/]*washingtonpost\.com\/']	
+videoregex = [r'/video/',
+        r'/post-live/']
+liveregex = [r'/post-live/']
+
+
