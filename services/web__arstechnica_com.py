92ca035675153ea679ddc289542b592a6d89423f
PressStartandSelect <attackedbyglitch@gmail.com>
2015-12-21 13:09:06 -0700

    add ars technica youtube channel

--- services/web__arstechnica_com.py
+++ services/web__arstechnica_com.py
@@ -1,10 +1,10 @@
 refresh = 4
-version = 20151221.01
+version = 20151221.02
 
 urls = ['http://feeds.arstechnica.com/arstechnica/index']
 regex = [r'^https?:\/\/[^\/]*arstechnica\.com\/',
 	    r'^https?:\/\/[^\/]*feedburner\.com\/']	
-videoregex = [r'^https?:\/\/(?:www\.)?youtube\.com\/embed\/']
+videoregex = []
 liveregex = [r'^https?:\/\/live\.arstechnica\.com\/']
 
 
---
+++ services/web__youtube_com_user_arstechnicavideos.py
@@ -0,0 +1,8 @@
+refresh = 5
+version = 20151221.01
+
+urls = ['https://www.youtube.com/user/arstechnicavideos/videos']
+regex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
+videoregex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
+liveregex = []
+
