f70a072b021a9a14ed974dead3bd5b2aa7ce3cf5
Justin Kerk <dopefishjustin@gmail.com>
2015-12-22 12:21:31 -0800

    Added Xinhua English & Chinese

---
+++ services/web__xinhuanet_com.py
@@ -0,0 +1,45 @@
+refresh = 4
+version = 20151222.01
+
+urls = ['http://www.xinhuanet.com/english/rss/worldrss.xml',
+        'http://www.xinhuanet.com/english/rss/chinarss.xml',
+        'http://www.xinhuanet.com/english/rss/businessrss.xml',
+        'http://www.xinhuanet.com/english/rss/culturerss.xml',
+        'http://www.xinhuanet.com/english/rss/sportsrss.xml',
+        'http://www.xinhuanet.com/english/rss/entertainmentrss.xml',
+        'http://www.xinhuanet.com/english/rss/scirss.xml',
+        'http://www.xinhuanet.com/english/rss/healthrss.xml',
+        'http://www.xinhuanet.com/english/rss/travelrss.xml',
+        'http://www.xinhuanet.com/english/rss/indepthrss.xml',
+        'http://www.xinhuanet.com/english/rss/oddrss.xml',
+        'http://www.xinhuanet.com/english/rss/photosrss.xml',
+        'http://www.xinhuanet.com/english/rss/videorss.xml',
+        'http://www.xinhuanet.com/politics/news_politics.xml',
+        'http://www.xinhuanet.com/world/news_world.xml',
+        'http://www.xinhuanet.com/local/news_province.xml',
+        'http://www.xinhuanet.com/mil/news_mil.xml',
+        'http://www.xinhuanet.com/photo/news_photo.xml',
+        'http://www.xinhuanet.com/tw/news_tw.xml',
+        'http://www.xinhuanet.com/overseas/news_overseas.xml',
+        'http://www.xinhuanet.com/finance/news_finance.xml',
+        'http://www.xinhuanet.com/fortune/news_fortune.xml',
+        'http://www.xinhuanet.com/house/news_house.xml',
+        'http://www.xinhuanet.com/legal/news_legal.xml',
+        'http://www.xinhuanet.com/edu/news_edu.xml',
+        'http://www.xinhuanet.com/sports/news_sports.xml',
+        'http://www.xinhuanet.com/ent/news_ent.xml',
+        'http://www.xinhuanet.com/auto/news_auto.xml',
+        'http://www.xinhuanet.com/collection/news_collection.xml',
+        'http://www.xinhuanet.com/travel/news_travel.xml',
+        'http://www.xinhuanet.com/tech/news_tech.xml',
+        'http://www.xinhuanet.com/abroad/news_abroad.xml',
+        'http://www.xinhuanet.com/food/news_food.xml',
+        'http://www.xinhuanet.com/health/news_health.xml',
+        'http://www.xinhuanet.com/shuhua/news_calligraphy.xml',
+        'http://www.xinhuanet.com/energy/news_energy.xml',
+        'http://www.xinhuanet.com/air/news_air.xml']
+regex = [r'^https?:\/\/[^\/]*xinhuanet\.com\/']	
+videoregex = [r'/video/']
+liveregex = []
+
+
