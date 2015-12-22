e8baf1f38def962928f0a8e26fb1a698f6e40c1c
PressStartandSelect <attackedbyglitch@gmail.com>
2015-12-21 20:48:17 -0700

    add the globe and mail and macleans

---
+++ services/web__macleans_ca.py
@@ -0,0 +1,9 @@
+refresh = 3
+version = 20151220.01
+
+urls = ['http://www.macleans.ca/feed/']
+regex = [r'^https?:\/\/[^\/]*macleans\.ca\/']
+videoregex = [r'^https?:\/\/[^\/]*brightcove\.com\/']
+liveregex = []
+
+
---
+++ services/web__theglobeandmail_com.py
@@ -0,0 +1,9 @@
+refresh = 3
+version = 20151221.01
+
+urls = ['http://www.theglobeandmail.com/?service=rss']
+regex = [r'^https?:\/\/[^\/]*theglobeandmail\.com\/']
+videoregex = [r'video/']
+liveregex = []
+
+
