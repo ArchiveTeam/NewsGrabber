cc454196f55f4d5e810508c671b7b29919bcd208
Tim Bowers <djsmiley2k@gmail.com>
2015-12-22 20:09:52 +0000

    Added coventry telegraph

---
+++ services/web__coventrytelegraph_net.py
@@ -0,0 +1,9 @@
+refresh = 4
+version = 20151221.01
+
+urls = ['http://www.coventrytelegraph.net/news/?service=rss']
+regex = [r'^https?:\/\/[^\/]*coventrytelegraph\.net\/news\/']
+videoregex = []
+liveregex = []
+
+
