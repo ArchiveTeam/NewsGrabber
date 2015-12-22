580a54755f491f9655138f72b2976409323fe2d5
HarryC145 <HarryC145@users.noreply.github.com>
2015-12-22 17:38:54 +0000

    added haaretz sites (israel)

---
+++ services/web__haaretz_co_il.py
@@ -0,0 +1,11 @@
+refresh = 5
+version = 20151222.1
+
+urls = ['http://www.haaretz.co.il/news',
+        'http://www.haaretz.com/news']
+
+regex = [r'^https?:\/\/[^\/]*haaretz\.co\.il\/',
+	    r'^https?:\/\/[^\/]*haaretz\.com\/']
+	    
+videoregex = []
+liveregex = ["/live/"]
