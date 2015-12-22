fd7d1382a612ec92989eb6d781d6748fb8d68cae
Arkiver2 <Arkiver@hotmail.com>
2015-12-22 01:14:56 +0100

    [web__aljazeera_com] add RSS feed

--- services/web__aljazeera_com.py
+++ services/web__aljazeera_com.py
@@ -1,7 +1,8 @@
 refresh = 7
 version = 20151221.01
 
-urls = ['http://www.aljazeera.com/news']
+urls = ['http://www.aljazeera.com/news',
+  'http://www.aljazeera.com/xml/rss/all.xml']
 regex = [r'^https?:\/\/[^\/]*aljazeera\.com']
 videoregex = []
 liveregex = []
