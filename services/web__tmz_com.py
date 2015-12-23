224d1d35bda50b7665a782f2be1c0c2f31fee50a
Arkiver2 <Arkiver@hotmail.com>
2015-12-23 13:47:42 +0100

    add tmz.com

---
+++ services/web__tmz_com.py
@@ -0,0 +1,10 @@
+refresh = 5
+version = 20151223.01
+
+urls = ['http://www.tmz.com/rss.xml',
+	'http://www.tmz.com/videos/']
+regex = [r'^https?:\/\/[^\/]*tmz\.com']	
+videoregex = [r'/videos/']
+liveregex = []
+
+
