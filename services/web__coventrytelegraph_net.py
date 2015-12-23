9f79221e6f9a29dde48a5cf5a91f0836e783e71c
Tim Bowers <djsmiley2k@gmail.com>
2015-12-23 19:32:37 +0000

    Changed to cover all article types not just news

--- services/web__coventrytelegraph_net.py
+++ services/web__coventrytelegraph_net.py
@@ -1,8 +1,8 @@
 refresh = 4
 version = 20151221.01
 
-urls = ['http://www.coventrytelegraph.net/news/?service=rss']
-regex = [r'^https?:\/\/[^\/]*coventrytelegraph\.net\/news\/']
+urls = ['http://www.coventrytelegraph.net/?service=rss']
+regex = [r'^https?:\/\/[^\/]*coventrytelegraph\.net\/']
 videoregex = []
 liveregex = []
 
