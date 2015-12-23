9f725f41ce6bd91a1f79038c43fbf959395d6b3b
Arkiver2 <Arkiver@hotmail.com>
2015-12-23 12:34:03 +0100

    4 german sites

---
+++ services/web__deutschlandradio_de.py
@@ -0,0 +1,10 @@
+refresh = 4
+version = 20151221.01
+
+urls = ['http://www.deutschlandradio.de/audio-archiv.260.de.html',
+	'http://www.deutschlandradio.de/']
+regex = [r'https?:\/\/[^\/]*deutschland', r'https?:\/\/[^\/]*dradio\.de']
+videoregex = [r'audio', r'video']
+liveregex = []
+
+
