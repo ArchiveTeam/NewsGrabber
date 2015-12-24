3ba9ef547e8fae24acf1d103f1180fb11acf775e
Arkiver2 <Arkiver@hotmail.com>
2015-12-24 02:03:04 +0100

    add metrohk.com.hk

---
+++ services/web__metrohk_com_hk.py
@@ -0,0 +1,10 @@
+refresh = 4
+version = 20151223.01
+
+urls = ['http://www.metrohk.com.hk/index.php',
+	'http://www.metrohk.com.hk/?cmd=instantNews',
+	'http://www.metrohk.com.hk/desktopRSS.php']
+regex = [r'https?:\/\/[^\/]*metrohk\.com\.hk']
+videoregex = []
+liveregex = []
+
