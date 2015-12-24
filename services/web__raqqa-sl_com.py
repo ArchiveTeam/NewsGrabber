cecaba032d9761b1908de2831991fd2b9ae93b13
PressStartandSelect <attackedbyglitch@gmail.com>
2015-12-24 15:52:43 -0700

    add raqqa-sl.com

---
+++ services/web__raqqa-sl_com.py
@@ -0,0 +1,10 @@
+refresh = 4
+version = 20151224.01
+
+urls = ['http://www.raqqa-sl.com/?feed=rss2',
+        'http://www.raqqa-sl.com/en/?feed=rss2']
+regex = [r'^https?:\/\/[^\/]*raqqa-sl\.com\/']	
+videoregex = []
+liveregex = []
+
+
