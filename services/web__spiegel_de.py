f00b471650b3af33155d0abe7b3f113b046c848c
Chris Egeland <chris@chrisegeland.com>
2015-12-24 09:21:05 -0500

    Add Der Spiegel
    Add Der Spiegel's German and English RSS feeds.

---
+++ services/web__spiegel_de.py
@@ -0,0 +1,8 @@
+refresh = 5
+version = 20151222
+
+urls = ['http://www.spiegel.de/schlagzeilen/index.rss',
+        'http://www.spiegel.de/international/index.rss']
+regex = [r'^http?:\/\/[^\/]*spiegel\.de\/']
+videoregex = [r'/video/']
+liveregex = []
