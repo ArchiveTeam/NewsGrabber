537c51dd7dedcf10af380a51322fcd5040554a44
HarryC145 <HarryC145@users.noreply.github.com>
2015-12-28 03:06:54 +0000

    Create web__theregister_co_uk.py

---
+++ services/web__theregister_co_uk.py
@@ -0,0 +1,8 @@
+refresh = 6 
+version = 20151228.01
+
+urls = ['http://www.theregister.co.uk/headlines.atom',
+        'http://www.theregister.co.uk']
+regex = [r'^https?:\/\/[^\/]*theregister\.co\.uk']
+videoregex = []
+liveregex = []
