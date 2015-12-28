1229ed02148bbadcef3f6c29ee3ab1c84f0c1430
Arkiver2 <Arkiver@hotmail.com>
2015-12-28 12:23:05 +0100

    add rtl7darts.nl

---
+++ services/web__rtl7darts_nl.py
@@ -0,0 +1,8 @@
+refresh = 7
+version = 20151228.01
+
+urls = ['http://www.rtl7darts.nl/nieuws',
+  'http://www.rtl7darts.nl/video']
+regex = [r'^https?:\/\/[^\/]*rtl7darts\.nl']
+videoregex = [r'video']
+liveregex = []
