56c316bce0468e9f6c944c9ecef53bb639763339
HarryC145 <HarryC145@users.noreply.github.com>
2015-12-28 01:54:00 +0000

    Yonhap News Agency

---
+++ services/web__yonhapnews_co_kr.py
@@ -0,0 +1,12 @@
+refresh = 4
+version = 20151228.01
+
+urls = ['http://yonhapnews.feedsportal.com/c/35025/f/647118/index.rss',
+        'http://www.yonhapnews.co.kr',
+        'http://english.yonhapnews.co.kr/']
+
+regex = [r'^https?:\/\/[^\/]*yonhapnews\.co\.kr\/',
+	    r'^https?:\/\/[^\/]*feedsportal\.com\/']
+	    
+videoregex = []
+liveregex = []
