7e1d5ccc0034cd95b90d9c902531db25e88bdfb8
Arkiver2 <Arkiver@hotmail.com>
2016-01-02 16:12:03 +0100

    2 UAE sites

---
+++ services/web__arabianbusiness_com.py
@@ -0,0 +1,8 @@
+refresh = 3
+version = 20160102.01
+
+urls = ['http://www.arabianbusiness.com/?service=rss']
+regex = [r'^https?:\/\/[^\/]*arabianbusiness\.com']
+videoregex = []
+liveregex = []
+
---
+++ services/web__moheet_com.py
@@ -0,0 +1,9 @@
+refresh = 3
+version = 20160102.01
+
+urls = ['http://www.moheet.com/rss/%D8%A2%D8%AE%D8%B1-%D8%A7%D9%84%D8%A3%D8%AE%D8%A8%D8%A7%D8%B1',
+	'http://www.moheet.com/']
+regex = [r'^https?:\/\/[^\/]*moheet\.com']
+videoregex = []
+liveregex = []
+
