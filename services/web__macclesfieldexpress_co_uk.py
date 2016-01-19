429af02427cc2b75f458f967823d2803232dd6d3
Tim Bowers <djsmiley2k@gmail.com>
2016-01-19 20:15:42 +0000

    fixed filename

---
+++ services/web__macclesfieldexpress_co_uk.py
@@ -0,0 +1,9 @@
+refresh = 8
+version = 20151223.01
+
+urls = ['http://www.macclesfield-express.co.uk/?service=rss']
+regex = [r'^https?:\/\/[^\/]*macclesfield-express\.co\.uk\/']
+videoregex = []
+liveregex = []
+
+
