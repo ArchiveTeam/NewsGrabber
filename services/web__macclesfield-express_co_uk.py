f5a471bff7d6d333cca02c8ffa2fe8c0d665c4bd
Tim Bowers <djsmiley2k@gmail.com>
2016-01-19 19:43:33 +0000

    fixed hyphen in filename

--- services/web__macclesfield-express_co_uk.py
+++
@@ -1,9 +0,0 @@
-refresh = 8
-version = 20151223.01
-
-urls = ['http://www.macclesfield-express.co.uk/?service=rss']
-regex = [r'^https?:\/\/[^\/]*macclesfield-express\.co\.uk\/']
-videoregex = []
-liveregex = []
-
-
