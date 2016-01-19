a73f56eb380defebe3d96f215c30bc3586a59040
Tim Bowers <djsmiley2k@gmail.com>
2016-01-19 20:14:38 +0000

    fixed filename

--- services/web__southportvisiter.co.uk.py
+++
@@ -1,9 +0,0 @@
-refresh = 7
-version = 20151223.01
-
-urls = ['http://www.southportvisiter.co.uk/?service=rss']
-regex = [r'^https?:\/\/[^\/]*southportvisiter\.co\.uk\/']
-videoregex = []
-liveregex = []
-
-
