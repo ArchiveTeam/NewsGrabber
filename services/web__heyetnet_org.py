24dc9c04d00e45d0c31f128dda94eae5b7c3c650
Justin Kerk <dopefishjustin@gmail.com>
2016-01-21 21:46:16 -0800

    Add some Iraqi sites

---
+++ services/web__heyetnet_org.py
@@ -0,0 +1,19 @@
+refresh = 10
+version = 20160121.01
+
+urls = ['http://heyetnet.org/en/index.php/heyetamsi/itemlist/?format=feed',
+        'http://heyetnet.org/en/index.php/guendem/itemlist/?format=feed',
+        'http://heyetnet.org/en/index.php/world/itemlist/?format=feed',
+        'http://heyetnet.org/en/index.php/raporlar/itemlist/?format=feed',
+		'http://heyetnet.org/en/index.php/aciklamalar/itemlist/?format=feed',
+		'http://heyetnet.org/en/index.php/articles/itemlist/?format=feed',
+		'http://heyetnet.org/tr/index.php/heyetamsi/itemlist/?format=feed',
+        'http://heyetnet.org/tr/index.php/gundem/itemlist/?format=feed',
+        'http://heyetnet.org/tr/index.php/raporlar/itemlist/?format=feed',
+		'http://heyetnet.org/tr/index.php/aciklamalar/itemlist/?format=feed',
+		'http://heyetnet.org/tr/index.php/makaleler/itemlist/?format=feed']
+regex = [r'^https?:\/\/[^\/]*heyetnet\.org\/']	
+videoregex = []
+liveregex = []
+
+
