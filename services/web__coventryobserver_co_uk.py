93ebaf02ea5553d57dfc4594c0e3cdf76ebe0955
Tim Bowers <djsmiley2k@gmail.com>
2016-01-23 20:22:36 +0000

    Added Coventry Observer

---
+++ services/web__coventryobserver_co_uk.py
@@ -0,0 +1,9 @@
+refresh = 7
+version = 20160123.01
+
+urls = ['http://coventryobserver.co.uk/rss-feed']
+regex = [r'^https?:\/\/[^\/]*coventryobserver\.co\.uk\/']
+videoregex = []
+liveregex = []
+
+
