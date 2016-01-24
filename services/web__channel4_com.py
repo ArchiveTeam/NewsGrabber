3a6e1bb97038d52b18afa98b2d0e0169997eac09
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-24 23:10:56 +0000

    Update web__channel4_com.py

--- services/web__channel4_com.py
+++ services/web__channel4_com.py
@@ -1,7 +1,7 @@
 refresh = 6
-version = 20160124.01
+version = 20160124.02
 
-urls = ['http://www/channel4.com/news/']
+urls = ['http://www.channel4.com/news/']
 regex = [r'^https?:\/\/[^\/]*channel4\.com\/']
 videoregex = [r'/videos/']
 liveregex = []
