bc8e3017e744d7e935c7443de10463e4b533aa24
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-24 22:47:43 +0000

    Create web__foxnews_com.py

---
+++ services/web__foxnews_com.py
@@ -0,0 +1,7 @@
+refresh = 4
+version = 20160124.01
+
+urls = ['http://www.foxnews.com']
+regex = [r'https?:\/\/[^\/]*foxnews\.com']
+videoregex = [r'https?:\/\/[^\/]*foxnews\.com\/v\/']
+liveregex = []
