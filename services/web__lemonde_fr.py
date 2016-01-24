3a58427158937cb72be75800e0cf9b012c403a87
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-24 22:44:36 +0000

    Create web__lemonde_fr.py

---
+++ services/web__lemonde_fr.py
@@ -0,0 +1,7 @@
+refresh = 5
+version = 20160124.01
+
+urls = ['http://www.lemonde.fr']
+regex = [r'https?:\/\/[^\/]*lemonde\.fr']
+videoregex = [r'https?:\/\/[^\/]*lemonde\.fr\/videos\/']
+liveregex = []
