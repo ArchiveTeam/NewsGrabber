6a2832b9b1b29636f235d0076bb9a6068cbdf4f3
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-25 19:50:41 +0000

    Create web__ft_com.py

---
+++ services/web__ft_com.py
@@ -0,0 +1,12 @@
+refresh = 4
+version = 20160125.01
+
+urls = ['http://www.ft.com/rss/home/asia',
+		'http://www.ft.com/rss/home/europe',
+		'http://www.ft.com/rss/home/india',
+		'http://www.ft.com/rss/home/middleeast',
+		'http://www.ft.com/rss/home/uk',
+		'http://www.ft.com/rss/home/us']
+regex = [r'^https?:\/\/[^\/]*ft\.com\/']
+videoregex = []r'^https?:\/\/[^\/]*video\.ft\.com\/']
+liveregex = []
