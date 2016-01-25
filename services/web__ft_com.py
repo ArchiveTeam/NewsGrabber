f39ee7341ac287af336007d530c32a3afd1c2f93
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-25 20:11:51 +0000

    Update web__ft_com.py

--- services/web__ft_com.py
+++ services/web__ft_com.py
@@ -8,5 +8,5 @@ urls = ['http://www.ft.com/rss/home/asia',
 		'http://www.ft.com/rss/home/uk',
 		'http://www.ft.com/rss/home/us']
 regex = [r'^https?:\/\/[^\/]*ft\.com\/']
-videoregex = []r'^https?:\/\/[^\/]*video\.ft\.com\/']
+videoregex = [r'^https?:\/\/[^\/]*video\.ft\.com\/']
 liveregex = []
