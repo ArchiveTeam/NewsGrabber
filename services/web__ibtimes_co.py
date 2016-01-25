43c8414dccadb7f2eeaefd9f03e18d3ad35de979
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-25 23:40:57 +0000

    Create web__ibtimes_co.py

---
+++ services/web__ibtimes_co.py
@@ -0,0 +1,13 @@
+refresh = 7
+version = 20160125.01
+
+urls = ['http://www.ibtimes.com.au',
+		'http://cn.ibtimes.com',
+		'http://www.ibtimes.co.in',
+		'http://it.ibtimes.com',
+		'http://jp.ibtimes.com',
+		'http://www.ibtimes.co.uk',
+		'http://www.ibtimes.com']
+regex = [r'^https?:\/\/[^\/]*ibtimes/.co\/]
+videoregex = []
+liveregex = []
