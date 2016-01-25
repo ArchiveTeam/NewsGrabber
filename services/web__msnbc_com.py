d6813d56e01c7a5c8c05e77b80f6c21c49d91806
Arkiver2 <Arkiver@hotmail.com>
2016-01-25 01:29:10 +0100

    msnbc.com

---
+++ services/web__msnbc_com.py
@@ -0,0 +1,8 @@
+refresh = 4
+version = 20160124.01
+
+urls = ['http://www.msnbc.com/',
+	'http://www.msnbc.com/feeds/latest']
+regex = [r'^https?:\/\/[^\/]*msnbc\.com']
+videoregex = [r'video', r'watch']
+liveregex = [r'live']
