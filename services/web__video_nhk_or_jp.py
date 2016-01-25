65407d4509ea763ca96c9adefb00218bfce489de
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-25 20:10:45 +0000

    Update web__video_nhk_or_jp.py

--- services/web__video_nhk_or_jp.py
+++ services/web__video_nhk_or_jp.py
@@ -2,6 +2,6 @@ refresh = 5
 version = 20151225.01
 
 urls = ['http://www3.nhk.or.jp/news/index.html?page=mov']
-regex = []
+regex = [r'^https?:\/\/[^\/]*nhk\.or\.jp']
 videoregex = [r'^https?:\/\/[^\/]*nhk\.or\.jp']
 liveregex = []
