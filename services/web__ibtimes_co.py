ab548f04cf4e074cc6fb81f07d73d725cf0e2697
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-26 00:36:26 +0000

    Update web__ibtimes_co.py

--- services/web__ibtimes_co.py
+++ services/web__ibtimes_co.py
@@ -1,5 +1,5 @@
 refresh = 7
-version = 20160125.01
+version = 20160126.01
 
 urls = ['http://www.ibtimes.com.au',
 		'http://cn.ibtimes.com',
@@ -8,6 +8,6 @@ urls = ['http://www.ibtimes.com.au',
 		'http://jp.ibtimes.com',
 		'http://www.ibtimes.co.uk',
 		'http://www.ibtimes.com']
-regex = [r'^https?:\/\/[^\/]*ibtimes/.co\/]
+regex = [r'^https?:\/\/[^\/]*ibtimes/.co\/']
 videoregex = []
 liveregex = []
