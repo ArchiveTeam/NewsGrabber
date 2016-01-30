73289725ada9aff28278a4312f9d31cb435ac745
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-30 22:17:57 +0000

    Update web__euronews_com.py

--- services/web__euronews_com.py
+++ services/web__euronews_com.py
@@ -3,12 +3,12 @@ version = 20160130.01
 
 urls = ['http://feeds.feedburner.com/euronews/en/home/',
         'http://feeds.feedburner.com/euronews/en/news/',
-    		'http://feeds.feedburner.com/euronews/en/business/',
-    		'http://feeds.feedburner.com/euronews/en/Euronews-NoComment/',
-    		'http://feeds.feedburner.com/euronews/en/europa/',
-    		'http://feeds.feedburner.com/euronews/en/sci-tech/',
-    		'http://feeds.feedburner.com/euronews/en/lifestyle/,
-    		'http://feeds.feedburner.com/euronews/en/picture-of-the-day/']
+    	'http://feeds.feedburner.com/euronews/en/business/',
+    	'http://feeds.feedburner.com/euronews/en/Euronews-NoComment/',
+    	'http://feeds.feedburner.com/euronews/en/europa/',
+    	'http://feeds.feedburner.com/euronews/en/sci-tech/',
+    	'http://feeds.feedburner.com/euronews/en/lifestyle/,
+    	'http://feeds.feedburner.com/euronews/en/picture-of-the-day/']
 regex = [r'^https?:\/\/[^\/]*euronews\.com\/']	
 videoregex = []
 liveregex = []
