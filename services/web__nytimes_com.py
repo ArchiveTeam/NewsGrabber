0d53f0af258a007d24c8969b322f193d77c11617
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-30 19:40:31 +0000

    Create web__nytimes_com.py

---
+++ services/web__nytimes_com.py
@@ -0,0 +1,16 @@
+refresh = 4
+version = 20160130.01
+
+urls = ['http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+        'http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml',
+		'http://rss.nytimes.com/services/xml/rss/nyt/World.xml',
+		'http://www.nytimes.com/services/xml/rss/nyt/US.xml',
+		'http://www.nytimes.com/services/xml/rss/nyt/NYRegion.xml',
+		'http://feeds.nytimes.com/nyt/rss/Business',
+		'http://feeds.nytimes.com/nyt/rss/Technology',
+		'http://www.nytimes.com/services/xml/rss/nyt/Sports.xml',
+		'http://www.nytimes.com/services/xml/rss/nyt/Science.xml',
+		'http://www.nytimes.com/services/xml/rss/nyt/Health.xml']
+regex = [r'^https?:\/\/[^\/]*nytimes\.com\/']	
+videoregex = ['video']
+liveregex = []
