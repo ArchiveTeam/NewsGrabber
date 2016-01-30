3ce5689ebb9db90d61c10498fd804a7821825deb
HarryC145 <HarryC145@users.noreply.github.com>
2016-01-30 21:58:00 +0000

    Create web__euronews_com.py

---
+++ services/web__euronews_com.py
@@ -0,0 +1,14 @@
+refresh = 5
+version = 20160130.01
+
+urls = ['http://feeds.feedburner.com/euronews/en/home/',
+        'http://feeds.feedburner.com/euronews/en/news/',
+    		'http://feeds.feedburner.com/euronews/en/business/',
+    		'http://feeds.feedburner.com/euronews/en/Euronews-NoComment/',
+    		'http://feeds.feedburner.com/euronews/en/europa/',
+    		'http://feeds.feedburner.com/euronews/en/sci-tech/',
+    		'http://feeds.feedburner.com/euronews/en/lifestyle/,
+    		'http://feeds.feedburner.com/euronews/en/picture-of-the-day/']
+regex = [r'^https?:\/\/[^\/]*euronews\.com\/']	
+videoregex = []
+liveregex = []
