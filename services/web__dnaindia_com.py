ad7ebce625b2568aca4115724d79b3e9050f556b
Mukesh <mukesh@ingenioustechie.com>
2016-02-16 19:20:26 +0530

    Added dnaindia.com

---
+++ services/web__dnaindia_com.py
@@ -0,0 +1,32 @@
+refresh = 10
+version = 20160216.01
+
+urls = ['http://www.dnaindia.com/feeds/analysis.xml',
+		'http://www.dnaindia.com/feeds/india.xml',
+		'http://www.dnaindia.com/feeds/world.xml',
+		'http://www.dnaindia.com/feeds/mumbai.xml',
+		'http://www.dnaindia.com/feeds/delhi.xml',
+		'http://www.dnaindia.com/feeds/bangalore.xml',
+		'http://www.dnaindia.com/feeds/pune.xml',
+		'http://www.dnaindia.com/feeds/ahmedabad.xml',
+		'http://www.dnaindia.com/feeds/sport.xml',
+		'http://www.dnaindia.com/feeds/money.xml',
+		'http://www.dnaindia.com/feeds/scitech.xml',
+		'http://www.dnaindia.com/feeds/entertainment.xml']
+regex = [r'^http?:\/\/[^\/]*dnaindia\.com']
+videoregex = []
+liveregex = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
