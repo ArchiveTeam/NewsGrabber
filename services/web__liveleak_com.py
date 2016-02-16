5f466cd1a106d72a6add8ae75aed2ab0a3ab7602
HarryC145 <HarryC145@users.noreply.github.com>
2016-02-16 19:36:09 +0000

    Create web__liveleak_com.py

---
+++ services/web__liveleak_com.py
@@ -0,0 +1,14 @@
+refresh = 4
+version = 20160216.01
+
+urls = ['http://www.liveleak.com/rss?channel_token=04c_1302956196',
+        'http://www.liveleak.com/rss?channel_token=1b3_1302956579',
+    		'http://www.liveleak.com/rss?channel_token=c39_1310110695',
+    		'http://www.liveleak.com/rss?channel_token=9ee_1303244161',
+    		'http://www.liveleak.com/rss?channel_token=b80_1390304670',
+    		'http://www.liveleak.com/rss?channel_token=cf3_1304149308',
+    		'http://www.liveleak.com/rss?channel_token=51a_1302956523',
+    		'http://www.liveleak.com/rss?featured=1']
+regex = [r'^https?:\/\/[^\/]*liveleak\.com']	
+videoregex = [r'^https?:\/\/[^\/]*liveleak\.com']
+liveregex = []
