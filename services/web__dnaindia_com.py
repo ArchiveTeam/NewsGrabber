a1cfc6bd64b7a4f997e60e165529a20dc872fd8a
Mukesh <mukesh@ingenioustechie.com>
2016-02-18 17:03:04 +0530

    did Indentation

--- services/web__dnaindia_com.py
+++ services/web__dnaindia_com.py
@@ -2,31 +2,19 @@ refresh = 6
 version = 20160216.01
 
 urls = ['http://www.dnaindia.com/feeds/analysis.xml',
-		'http://www.dnaindia.com/feeds/india.xml',
-		'http://www.dnaindia.com/feeds/world.xml',
-		'http://www.dnaindia.com/feeds/mumbai.xml',
-		'http://www.dnaindia.com/feeds/delhi.xml',
-		'http://www.dnaindia.com/feeds/bangalore.xml',
-		'http://www.dnaindia.com/feeds/pune.xml',
-		'http://www.dnaindia.com/feeds/ahmedabad.xml',
-		'http://www.dnaindia.com/feeds/sport.xml',
-		'http://www.dnaindia.com/feeds/money.xml',
-		'http://www.dnaindia.com/feeds/scitech.xml',
-		'http://www.dnaindia.com/feeds/entertainment.xml']
+	'http://www.dnaindia.com/feeds/india.xml',
+	'http://www.dnaindia.com/feeds/world.xml',
+	'http://www.dnaindia.com/feeds/mumbai.xml',
+	'http://www.dnaindia.com/feeds/delhi.xml',
+	'http://www.dnaindia.com/feeds/bangalore.xml',
+	'http://www.dnaindia.com/feeds/pune.xml',
+	'http://www.dnaindia.com/feeds/ahmedabad.xml',
+	'http://www.dnaindia.com/feeds/sport.xml',
+	'http://www.dnaindia.com/feeds/money.xml',
+	'http://www.dnaindia.com/feeds/scitech.xml',
+	'http://www.dnaindia.com/feeds/entertainment.xml']
 regex = [r'^http?:\/\/[^\/]*dnaindia\.com']
 videoregex = []
 liveregex = []
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+wikidata = 'Q2055675'
