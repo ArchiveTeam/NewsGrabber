b9762e546f00bf633b71993d2c0d68ee5e4e0b96
PressStartandSelect <attackedbyglitch@gmail.com>
2016-03-06 17:12:20 -0700

    add nunatsiaqonline.ca

---
+++ services/web__nunatsiaqonline_ca.py
@@ -0,0 +1,9 @@
+refresh = 6
+version = 20160306.01
+
+urls = ['http://www.nunatsiaqonline.ca/stories/rss']
+regex = [r'^https?:\/\/[^\/]*nunatsiaqonline\.ca\/']	
+videoregex = []
+liveregex = []
+
+
