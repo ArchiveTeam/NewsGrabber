ca2ddb8c12ad144a87e942843ec998f41d56f038
BnA-Robin <yorick@0xf.nl>
2016-03-13 22:04:26 +0100

    Fix alignment and filename for qq.com

---
+++ services/web__qq_com.py
@@ -0,0 +1,43 @@
+refresh = 5
+version = 20160313.01
+
+urls = ['http://www.qq.com',
+	'http://news.qq.com/',
+	'http://finance.qq.com/',
+	'http://stock.qq.com/',
+	'http://news.qq.com/photo.shtml',
+	'http://mil.qq.com/mil_index.htm',
+	'http://finance.qq.com/money/',
+	'http://sports.qq.com/',
+	'http://v.qq.com/',
+	'http://v.qq.com/tv/',
+	'http://v.qq.com/variety/',
+	'http://ent.qq.com/',
+	'http://fashion.qq.com/',
+	'http://health.qq.com/',
+	'http://ent.qq.com/star/',
+	'http://baby.qq.com/',
+	'http://house.qq.com/',
+	'http://auto.qq.com/',
+	'http://www.jia360.com/',
+	'http://digi.tech.qq.com/hea/',
+	'http://tech.qq.com/',
+	'http://games.qq.com/',
+	'http://kid.qq.com/',
+	'http://digi.tech.qq.com/',
+	'http://digi.tech.qq.com/mobile/',
+	'http://astro.fashion.qq.com/',
+	'http://cul.qq.com/',
+	'http://edu.qq.com/',
+	'http://class.qq.com/',
+	'http://dajia.qq.com/',
+	'http://book.qq.com/',
+	'http://edu.qq.com/abroad/',
+	'http://gongyi.qq.com/',
+	'http://v.qq.com/paike/',
+	'http://foxue.qq.com/']
+regex = [r'https?:\/\/[^\/]*qq\.com']
+videoregex = []
+liveregex = []
+
+wikidata = 'Q857640'
