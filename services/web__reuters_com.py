b80703941bb5869338343123cc3a84478bbe7195
HarryC145 <HarryC145@users.noreply.github.com>
2016-03-19 15:24:10 +0000

    Update web__reuters_com.py

--- services/web__reuters_com.py
+++ services/web__reuters_com.py
@@ -1,7 +1,22 @@
 refresh = 3
 version = 20160124.01
 
-urls = ['http://www.reuters.com/']
+urls = ['http://www.reuters.com',
+        'http://af.reuters.com',
+        'http://ara.reuters.com',
+        'http://ar.reuters.com',
+        'http://br.reuters.com',
+        'http://ca.reuters.com',
+        'http://fr.reuters.com',
+        'http://de.reuters.com',
+        'http://in.reuters.com',
+        'http://it.reuters.com',
+        'http://jp.reuters.com',
+        'http://lta.reuters.com',
+        'http://mx.reuters.com',
+        'http://ru.reuters.com',
+        'http://es.reuters.com',
+        'http://uk.reuters.com']
 regex = [r'^https?:\/\/[^\/]*reuters\.com']
 videoregex = [r'video']
 liveregex = []
