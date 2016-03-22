9f6d7d0c59919aacfef896de92948992f51e77c3
BnA-Robin <yorick@0xf.nl>
2016-03-23 00:13:57 +0100

    Added russian ria.ru

---
+++ services/web__ria_ru.py
@@ -0,0 +1,11 @@
+refresh = 8
+version = 20160323.01
+
+urls = [
+	'http://ria.ru/',
+	'http://ria.ru/export/rss2/index.xml']
+regex = [r'^https?:\/\/[^\/]*ria\.ru']
+videoregex = []
+liveregex = []
+
+
