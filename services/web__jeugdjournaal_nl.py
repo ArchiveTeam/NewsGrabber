7d43d5b9aa475788a3d38a01272589c23a98bcc7
BnA-Robin <yorick@0xf.nl>
2016-03-23 19:24:45 +0100

    Added special Dutch news website for children

---
+++ services/web__jeugdjournaal_nl.py
@@ -0,0 +1,9 @@
+refresh = 7
+version = 20160323.01
+
+urls = ['http://www.jeugdjournaal.nl/',
+	'http://jeugdjournaal.nl/uitzendingen/']
+regex = [r'^https?:\/\/[^\/]*jeugdjournaal\.nl']
+videoregex = [r'^https?:\/\/[^\/]*jeugdjournaal\.nl\/uitzending\/']
+liveregex = []
+
