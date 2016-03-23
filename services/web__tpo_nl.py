d75e8b4f9e6182e5d2cbc1ef7ae2711ec798f4cd
BnA-Robin <yorick@0xf.nl>
2016-03-23 01:01:20 +0100

    Added some more services that got referenced in recent news and/or have background articles

---
+++ services/web__tpo_nl.py
@@ -0,0 +1,17 @@
+refresh = 6
+version = 20160323.01
+
+urls = [
+	'http://tpo.nl/',
+	'http://politiek.tpo.nl/',
+	'http://media.tpo.nl/',
+	'http://nieuws.tpo.nl/',
+	'http://biz.tpo.nl/',
+	'http://chroom.tpo.nl/',
+	'http://tpo.nl/columns/',
+	'http://regio.tpo.nl/',
+	'http://cult.tpo.nl/',
+	'http://talk.tpo.nl/']
+regex = [r'^https?:\/\/[^\/]*tpo\.nl']
+videoregex = []
+liveregex = []
