469211dd711c5dd647b45acc6e840a2ed7aae8d8
BnA-Robin <yorick@0xf.nl>
2016-03-28 23:07:36 +0200

    Think Tanks services: Albania services

---
+++ services/web__aiis-albania_org.py
@@ -0,0 +1,13 @@
+refresh = 8
+version = 20160328.01
+
+urls = ['http://www.aiis-albania.org/']
+regex = [r'^https?:\/\/[^\/]*aiis-albania\.org',
+	'http://www.aiis-albania.org/?q=publications/books',
+	'http://www.aiis-albania.org/?q=publications/policy-papers',
+	'http://www.aiis-albania.org/?q=publications/working-papers',
+	'http://www.aiis-albania.org/?q=reports',
+	'http://www.aiis-albania.org/?q=publications/essays',
+	'http://www.aiis-albania.org/?q=ir-library']
+videoregex = []
+liveregex = []
