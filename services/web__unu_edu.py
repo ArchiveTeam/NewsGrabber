c47247dd89d88c8dc056d24152af3e29744d1465
BnA-Robin <yorick@0xf.nl>
2016-03-28 23:06:46 +0200

    Think Tanks services: United Nations services

---
+++ services/web__unu_edu.py
@@ -0,0 +1,15 @@
+refresh = 7
+version = 20160328.01
+
+urls = ['http://unu.edu/',
+	'http://unu.edu/publications/articles',
+	'http://unu.edu/news',
+	'http://collections.unu.edu/list/latest/',
+	'http://collections.unu.edu/list/?cat=quick_filter&sort_by=searchKey0&search_keys[0]=policy+briefs',
+	'http://collections.unu.edu/collection/UNU:4',
+	'http://unu.edu/publications/annual-reports',
+	'http://ourworld.unu.edu/en/',
+	'http://collections.unu.edu/list/?search_keys[0]=working+papers&cat=quick_filter&sort_by=searchKey0']
+regex = [r'^https?:\/\/[^\/]*unu\.edu']
+videoregex = []
+liveregex = []
