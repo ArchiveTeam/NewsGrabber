ed09aeb61ee766e1e2692e9d0c7aae4e96af89b6
BnA-Robin <yorick@0xf.nl>
2016-04-01 00:59:58 +0200

    Update web__percapita_org_au.py
    Typo

--- services/web__percapita_org_au.py
+++ services/web__percapita_org_au.py
@@ -5,6 +5,6 @@ urls = ['http://percapita.org.au/',
 	'http://percapita.org.au/media/',
 	'http://percapita.org.au/events/',
 	'http://percapita.org.au/research/']
-regex = [r'^https?:\/\/[^\/]*percapita\.org'\.au]
+regex = [r'^https?:\/\/[^\/]*percapita\.org\.au']
 videoregex = []
 liveregex = []
