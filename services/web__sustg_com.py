8cea13d71f414916ae48f725913584771249e401
Arkiver2 <Arkiver@hotmail.com>
2016-04-01 23:18:23 +0200

    Add sustg.com.

---
+++ services/web__sustg_com.py
@@ -0,0 +1,17 @@
+refresh = 8
+version = 20160401.01
+
+urls = ['http://sustg.com/',
+	'http://sustg.com/the-review/',
+	'http://sustg.com/analysis/',
+	'http://sustg.com/focus-ksa/',
+	'http://sustg.com/photos/',
+	'http://sustg.com/newsroom/economy/',
+	'http://sustg.com/newsroom/policy/',
+	'http://sustg.com/newsroom/society/',
+	'http://sustg.com/newsroom/security/',
+	'http://sustg.com/newsroom/sectors/',
+	'http://sustg.com/newsroom/geographic/']
+regex = [r'^https?:\/\/[^\/]*sustg\.com']
+videoregex = []
+liveregex = []
