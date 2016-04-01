42530d8685b047e070006b8fc67a9348481f9380
HarryC145 <HarryC145@users.noreply.github.com>
2016-04-01 23:11:40 +0100

    Create web__lanacion_com_ar.py

---
+++ services/web__lanacion_com_ar.py
@@ -0,0 +1,41 @@
+refresh = 5
+version = 20160401.01
+
+urls = ['http://www.lanacion.com.ar/',
+	      'http://www.lanacion.com.ar/comunidad-de-negocios/',
+	      'http://www.lanacion.com.ar/consumo-t46838/',
+	      'http://www.lanacion.com.ar/finanzas-personales-t48750/',
+	      'http://www.lanacion.com.ar/emprendedores-t53673/',
+	      'http://www.lanacion.com.ar/campo-t29405/',
+	      'http://www.lanacion.com.ar/emprendedores-t53673/',
+	      'http://www.lanacion.com.ar/dolar-hoy-t1369/',
+	      'http://www.lanacion.com.ar/vida-y-ocio/',
+	      'http://www.lanacion.com.ar/ideas/',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/origen=2',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/origen=1',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=30',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=272',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=131',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7773',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7775',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7774',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=28',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=120',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=494',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=421',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=347',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=432',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=504',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=371',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=337',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=1312',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=1363',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=1348',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=1344',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=6734',
+	      'http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=6373']
+	      
+regex = [r'^https?:\/\/[^\/]*lanacion\.com\.ar\/']
+videoregex = []
+liveregex = []
