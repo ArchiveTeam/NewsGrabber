a7f345a47e0374ad60562b59c89e03f351fa199b
HarryC145 <HarryC145@users.noreply.github.com>
2016-04-01 20:19:15 +0100

    Create web__bloomberg_com.py

---
+++ services/web__bloomberg_com.py
@@ -0,0 +1,19 @@
+refresh = 4
+version = 20160401.01
+
+urls = ['http://www.bloomberg.com/europe',
+	'http://www.bloomberg.com',
+	'http://www.bloomberg.com/markets',
+	'http://www.bloomberg.com/technology',
+	'http://www.bloomberg.com/politics',
+	'http://www.bloomberg.com/pursuits',
+	'http://www.bloomberg.com/businessweek',
+	'http://www.bloomberg.com/news/science-energy',
+	'http://www.bloomberg.com/graphics',
+	'http://www.bloomberg.com/game-plan',
+	'http://www.bloomberg.com/small-business',
+	'http://www.bloomberg.com/personal-finance',
+	'http://www.bloomberg.com/asia']
+regex = [r'^https?:\/\/[^\/]*bloomberg\.com\/']	
+videoregex = ["/videos/"]
+liveregex = []
