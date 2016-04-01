7c215dbebc4b9f2546768061c101190196cdf14c
HarryC145 <HarryC145@users.noreply.github.com>
2016-04-01 20:31:28 +0100

    Create web__bloomberg_co_jp.py

---
+++ services/web__bloomberg_co_jp.py
@@ -0,0 +1,21 @@
+refresh = 4
+version = 20160401.01
+
+urls = ['https://www.bloomberg.co.jp',
+	'https://www.bloomberg.co.jp/news/overseas',
+	'https://www.bloomberg.co.jp/news/domestic',
+	'https://www.bloomberg.co.jp/news/exclusive',
+	'https://www.bloomberg.co.jp/news/markets',
+	'https://www.bloomberg.co.jp/news/industries',
+	'https://www.bloomberg.co.jp/news/economy',
+	'https://www.bloomberg.co.jp/news/commentary',
+	'https://www.bloomberg.co.jp/markets/stocks',
+	'https://www.bloomberg.co.jp/markets/stocks/futures',
+  'https://www.bloomberg.co.jp/markets/regions/asia-pacific',
+  'https://www.bloomberg.co.jp/markets/rates-bonds',
+  'https://www.bloomberg.co.jp/markets/currencies',
+  'https://www.bloomberg.co.jp/markets/commodities']
+	
+regex = [r'^https?:\/\/[^\/]*bloomberg\.co\.jp']
+videoregex = []
+liveregex = ["/live/"]
