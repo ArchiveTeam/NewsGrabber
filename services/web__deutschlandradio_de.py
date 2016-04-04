e9f741f37d3509baee18d93a2ab627fd80794292
Arkiver2 <Arkiver@hotmail.com>
2016-04-04 14:08:03 +0200

    Add domain as first URL in 'urls'.

--- services/web__deutschlandradio_de.py
+++ services/web__deutschlandradio_de.py
@@ -1,8 +1,8 @@
 refresh = 4
-version = 20151221.01
+version = 20160403.01
 
-urls = ['http://www.deutschlandradio.de/audio-archiv.260.de.html',
-	'http://www.deutschlandradio.de/']
+urls = ['http://www.deutschlandradio.de/',
+	'http://www.deutschlandradio.de/audio-archiv.260.de.html']
 regex = [r'https?:\/\/[^\/]*deutschland', r'https?:\/\/[^\/]*dradio\.de']
 videoregex = [r'audio', r'video']
 liveregex = []
--- services/web__donaldjtrump_com.py
+++ services/web__donaldjtrump_com.py
@@ -1,7 +1,8 @@
 refresh = 10
-version = 20151221.01
+version = 20160403.01
 
-urls = ['http://www.donaldjtrump.com/media/']
+urls = ['http://www.donaldjtrump.com/',
+	'http://www.donaldjtrump.com/media/']
 regex = [r'^https?:\/\/[^\/]*donaldjtrump\.com\/']	
 videoregex = [r'^https?:\/\/(?:www\.)?youtube\.com\/embed\/']
 liveregex = []
