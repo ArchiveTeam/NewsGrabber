45a409e9181a9b0cd37de353ce59da442fb34946
HarryC145 <HarryC145@users.noreply.github.com>
2016-04-06 00:09:41 +0100

    Create web__irna_ir.py

---
+++ services/web__irna_ir.py
@@ -0,0 +1,18 @@
+refresh = 5
+version = 20160406.01
+
+urls = ['http://www.irna.ir',
+	      'http://www.irna.ir/fa/page/225/%D9%81%DB%8C%D9%84%D9%85-%D9%88-%D8%B5%D8%AF%D8%A7',
+	      'http://www.irna.ir/fa/51/%D8%B9%DA%A9%D8%B3',
+	      'http://www.irna.ir/fa/services/212/%D8%AD%D9%88%D8%A7%D8%AF%D8%AB',
+	      'http://www.irna.ir/fa/services/103/%D9%BE%DA%98%D9%88%D9%87%D8%B4',
+	      'http://www.irna.ir/fa/services/54/%D8%A7%D8%B3%D8%AA%D8%A7%D9%86%D9%87%D8%A7',
+	      'http://www.irna.ir/fa/services/180/%D8%B9%D9%84%D9%85%DB%8C',
+	      'http://www.irna.ir/fa/services/32/%D8%A7%D8%AC%D8%AA%D9%85%D8%A7%D8%B9%DB%8C',
+	      'http://www.irna.ir/fa/services/5/%D8%B3%DB%8C%D8%A7%D8%B3%DB%8C',
+	      'http://www.irna.ir/en/',
+	      'http://www.irna.ir/en/telex.aspx?area=0',
+	      'http://www.irna.ir/fa/rss.aspx?kind=-1']
+regex = [r'^https?:\/\/[^\/]*irna\.ir']
+videoregex = []
+liveregex = []
