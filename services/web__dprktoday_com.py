0b10f81a36ad45b4050f06fb82d2bcaa1f67e161
HarryC145 <HarryC145@users.noreply.github.com>
2016-04-05 13:27:45 +0100

    Create web__dprktoday_com.py

---
+++ services/web__dprktoday_com.py
@@ -0,0 +1,8 @@
+refresh = 7
+version = 20160405.01
+
+urls = ['http://www.dprktoday.com/',
+        'http://www.dprktoday.com/rss.php']
+regex = [r'^https?:\/\/[^\/]*dprktoday\.com']
+videoregex = []
+liveregex = []
