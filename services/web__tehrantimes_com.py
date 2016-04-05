b5d5208f163953681621c799fc64999b0249c2eb
HarryC145 <HarryC145@users.noreply.github.com>
2016-04-05 23:55:30 +0100

    Create web__tehrantimes_com.py

---
+++ services/web__tehrantimes_com.py
@@ -0,0 +1,13 @@
+refresh = 6
+version = 20160405.01
+
+urls = ['http://www.tehrantimes.com',
+        'http://www.tehrantimes.com/service/social',
+        'http://www.tehrantimes.com/service/economy',
+        'http://www.tehrantimes.com/service/politics',
+        'http://www.tehrantimes.com/service/sports',
+        'http://www.tehrantimes.com/service/culture',
+        'http://www.tehrantimes.com/rss-homepage']
+regex = [r'^https?:\/\/[^\/]*tehrantimes\.com\/']
+videoregex = [r'/multimedia/']
+liveregex = []
