4f9ba03e2b364d05e3b0453968dd7eda0698c904
HarryC145 <HarryC145@users.noreply.github.com>
2016-04-05 15:04:21 +0100

    Create web__leparisien_fr.py

---
+++ services/web__leparisien_fr.py
@@ -0,0 +1,13 @@
++refresh = 4
+ +version = 20160405.01
+ +
+ +urls = ['http://www.leparisien.fr/',
+ +        'http://www.leparisien.fr/sports/actualite-sports',
+          'http://www.leparisien.fr/info-paris-ile-de-france-oise.php',
+          'http://www.leparisien.fr/loisirs-spectacles/actualites-informations-videos-loisirs-spectacles.php',
+          'http://videos.leparisien.fr',
+          'http://www.leparisien.fr/diaporamas/photos',
+          'http://www.leparisien.fr/actualites-a-la-une.rss.xml#xtor=RSS-1481423633']
+ +regex = [r'^https?:\/\/[^\/]*leparisien\.fr']
+ +videoregex = ['\/videos\/']
+ +liveregex = []
