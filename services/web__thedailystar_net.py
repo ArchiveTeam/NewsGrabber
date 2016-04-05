3091424dbae4bef097e1b065ebcc80ed2c47dfdd
HarryC145 <HarryC145@users.noreply.github.com>
2016-04-06 00:03:03 +0100

    Create web__thedailystar_net.py

---
+++ services/web__thedailystar_net.py
@@ -0,0 +1,18 @@
+refresh = 6
+version = 20160406.01
+
+urls = ['http://www.thedailystar.net/',
+	      'http://www.thedailystar.net/newspaper',
+	      'http://www.thedailystar.net/business',
+	      'http://www.thedailystar.net/op-ed',
+	      'http://www.thedailystar.net/sports',
+	      'http://www.thedailystar.net/arts-entertainment',
+	      'http://www.thedailystar.net/lifestyle',
+	      'http://www.thedailystar.net/bytes',
+	      'http://www.thedailystar.net/showbiz',
+	      'http://www.thedailystar.net/shout',
+	      'http://www.thedailystar.net/most-popular-stories/rss.xml',
+	      'http://www.thedailystar.net/top-news/rss.xml']
+regex = [r'^https?:\/\/[^\/]*thedailystar\.net']
+videoregex = []
+liveregex = []
