94094330f5ce30a330afa1229af35b3b11f68724
sollidius <sollidius@users.noreply.github.com>
2016-05-20 12:34:46 +0200

    Create web__gva_be.py

---
+++ services/web__gva_be.py
@@ -0,0 +1,9 @@
+refresh = 6
+version = 20160520.01
+
+urls = ['https://gva.be', 
+        'http://www.gva.be/rss/section/ca750cdf-3d1e-4621-90ef-a3260118e21c',
+        'http://www.gva.be/video']
+regex = [r'^https?:\/\/[^\/]*gva\.be']
+videoregex = []
+liveregex = []
