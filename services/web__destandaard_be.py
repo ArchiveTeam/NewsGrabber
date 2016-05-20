ab2ea425cc2e2c8b7e999fed4fb1ce836967badc
sollidius <sollidius@users.noreply.github.com>
2016-05-20 12:42:08 +0200

    Create web__destandaard_be.py

---
+++ services/web__destandaard_be.py
@@ -0,0 +1,35 @@
+
+refresh = 6
+version = 20162005.01
+
+urls = ['http://www.standaard.be',
+  'http://www.standaard.be/rss/section/1f2838d4-99ea-49f0-9102-138784c7ea7c',
+  'http://www.standaard.be/rss/section/e70ccf13-a2f0-42b0-8bd3-e32d424a0aa0',
+  'http://www.standaard.be/rss/section/ab8d3fd8-bf2f-487a-818b-9ea546e9a859',
+  'http://www.standaard.be/rss/section/eb1a6433-ca3f-4a3b-ab48-a81a5fb8f6e2',
+  'http://www.standaard.be/rss/section/451c8e1e-f9e4-450e-aa1f-341eab6742cc',
+  'http://www.standaard.be/rss/section/8f693cea-dba8-46e4-8575-807d1dc2bcb7',
+  'http://www.standaard.be/rss/section/113a9a78-f65a-47a8-bd1c-b24483321d0f',
+  'http://www.standaard.be/rss/section/3a4b39a1-e58f-42e4-8ae9-a0f90f97f27f',
+  'http://www.standaard.be/rss/section/51dd6a40-e297-409c-af25-9f0301159a1c',
+  'http://www.standaard.be/rss/section/ec1dbffa-a00b-48e6-96f0-00d215f90744',
+  'http://www.standaard.be/rss/section/eed96e23-ed90-4818-83ab-adabf8caf0f4',
+  'http://www.standaard.be/rss/section/f4dd4e8d-6cb1-4eef-abc2-06b0e3d72de4',
+  'http://www.standaard.be/rss/section/a166bb48-b6b4-4c1a-beb3-9f0301160b75',
+  'http://www.standaard.be/rss/section/06b5429e-beb1-4e76-909c-9f0301162a9c',
+  'http://www.standaard.be/rss/section/246d27cb-ce7b-4245-bad4-a09f0119b450',
+  'http://www.standaard.be/rss/section/a30afc42-3737-4301-8f8a-5b6833855457',
+  'http://www.standaard.be/rss/section/212b8b54-bd91-4c8b-942c-8029e8797d36',
+  'http://www.standaard.be/rss/section/6aa8d4fa-4b9a-40d5-aa8f-87ac72472f27',
+  'http://www.standaard.be/rss/section/46025691-2ec4-4a06-b6d7-9773686a24a7',
+  'http://www.standaard.be/rss/section/9bdf4a14-f8bf-4439-aaf1-344181f73e73',
+  'http://www.standaard.be/rss/section/270b7f8f-dd73-44cb-b622-9f7200a439a7',
+  'http://www.standaard.be/rss/tag/tv-blog',
+  'http://www.standaard.be/rss/tag/marketingblog',
+  'http://www.standaard.be/rss/tag/blog-en-nu-even-elders',
+  'http://www.standaard.be/rss/tag/beursexperts',
+  'http://www.standaard.be/rss/tag/autoblog'
+	]
+regex = [r'^https?:\/\/[^\/]*standaard\.be']
+videoregex = []
+liveregex = []
