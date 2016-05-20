b464e56d430be942d76943330e73972be579c7f1
sollidius <sollidius@users.noreply.github.com>
2016-05-20 22:29:33 +0200

    Update web__lesoir_be.py
    fixed

--- services/web__lesoir_be.py
+++ services/web__lesoir_be.py
@@ -9,8 +9,16 @@ urls = [
   'http://www.lesoir.be/feed/Economie/destination_principale_block',
   'http://www.lesoir.be/feed/D%C3%A9bats/destination_debats_principal',
   'http://www.lesoir.be/feed/Univers/Victoire/default_univers_block',
-  'http://www.lesoir.be/feed/Univers/Soirmag/default_univers_block'
+  'http://www.lesoir.be/feed/Univers/Soirmag/default_univers_block',
+  'http://www.lesoir.be/2/sports',
+  'http://www.lesoir.be/3/culture',
+  'http://www.lesoir.be/4/economie',
+  'http://www.lesoir.be/5/d%C3%A9bats',
+  'http://blog.lesoir.be/',
+  'http://portfolio.lesoir.be/main.php',
+  'http://www.lesoir.be/102446/soirmag'
   ]
-regex = [r'^https?:\/\/[^\/]*lesoir\.be', r'^https?:\/\/[^\/]*rosselcdn\.net',]
+regex = [r'^https?:\/\/[^\/]*lesoir\.be', 
+  r'^https?:\/\/[^\/]*rosselcdn\.net']
 videoregex = []
 liveregex = []
