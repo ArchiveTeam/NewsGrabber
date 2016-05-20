805420b5a0b0a9389b99c608170bb902747cfd79
sollidius <sollidius@users.noreply.github.com>
2016-05-20 12:31:43 +0200

    Update web__hln_be.py

--- services/web__hln_be.py
+++ services/web__hln_be.py
@@ -1,7 +1,9 @@
 refresh = 6
 version = 20160520.01
 
-urls = ['http://www.hln.be/rss.xml']
+urls = ['https://hln.be', 
+        'http://www.hln.be/rss.xml',
+        'http://www.hln.be/hln/nl/3384/video/index.dhtml']
 regex = [r'^https?:\/\/[^\/]*hln\.be']
 videoregex = []
 liveregex = []
