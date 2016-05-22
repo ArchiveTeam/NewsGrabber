f7ffe9e9d7b54fd44af50eb9f9e08412ed9aad75
HarryC145 <HarryC145@users.noreply.github.com>
2016-05-22 08:21:18 +0100

    Update web__demorgen_be.py

--- services/web__demorgen_be.py
+++ services/web__demorgen_be.py
@@ -3,7 +3,7 @@ refresh = 6
 version = 20162005.01
 
 urls = ['http://www.demorgen.be',
-  'http://www.demorgen.be/nieuws/rss.xml',
+  	'http://www.demorgen.be/nieuws/rss.xml',
 	'http://www.demorgen.be/binnenland/rss.xml',
 	'http://www.demorgen.be/buitenland/rss.xml',
 	'http://www.demorgen.be/muziek/rss.xml',
@@ -11,8 +11,7 @@ urls = ['http://www.demorgen.be',
 	'http://www.demorgen.be/nieuws/rss.xml',
 	'http://www.demorgen.be/economie/rss.xml',
 	'http://www.demorgen.be/wetenschap/rss.xml',
-	'http://www.demorgen.be/sport/rss.xml',
-	]
+	'http://www.demorgen.be/sport/rss.xml']
 regex = [r'^https?:\/\/[^\/]*demorgen\.be']
 videoregex = []
 liveregex = []
