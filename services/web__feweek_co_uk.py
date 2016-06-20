4bcdf2348f362b657d7b7e3ef1c0cf2945fc4549
HarryC145 <HarryC145@users.noreply.github.com>
2016-06-20 23:31:01 +0100

    Create web__feweek_co_uk.py

---
+++ services/web__feweek_co_uk.py
@@ -0,0 +1,13 @@
+refresh = 6
+version = 20160620.01
+
+urls = ['http://feweek.co.uk/',
+	'http://feweek.co.uk/category/profiles/',
+	'http://feweek.co.uk/category/expert/',
+	'http://feweek.co.uk/category/moversandshakers/',
+	'http://feweek.co.uk/category/campus/',
+	'http://feweek.co.uk/category/supplements/',
+	'http://feweek.co.uk/downloads/']
+regex = [r'^https?:\/\/[^\/]*feweek\.co\.uk\/']	
+videoregex = []
+liveregex = []
