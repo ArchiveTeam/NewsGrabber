045bf6bb824bcc5eed39a6b3c4f07dc934fa3cd1
HarryC145 <HarryC145@users.noreply.github.com>
2016-06-20 23:23:43 +0100

    Create web__fullfact_org.py

---
+++ services/web__fullfact_org.py
@@ -0,0 +1,13 @@
+refresh = 6
+version = 20160620.01
+
+urls = ['https://fullfact.org/',
+	'https://fullfact.org/europe/',
+	'https://fullfact.org/health/',
+	'https://fullfact.org/crime/',
+	'https://fullfact.org/education/',
+	'https://fullfact.org/immigration/',
+	'https://fullfact.org/law/']
+regex = [r'^https?:\/\/[^\/]*fullfact\.org']
+videoregex = []
+liveregex = []
