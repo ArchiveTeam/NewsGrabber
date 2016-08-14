228665007d5742f772573122ba07a904c153f9cd
Jesse Weinstein <jesse@wefu.org>
2016-08-14 14:35:58 -0700

    Fix typo

--- services/web__viceland_com.py
+++ services/web__viceland_com.py
@@ -4,4 +4,5 @@ version = 20160402.01
 urls = ['https://www.viceland.com/']
 regex = [r'^https?:\/\/[^\/]*viceland\.com']
 videoregex = ['\/video\/']
-liveregex = []wikidata = 'Q23022864'
+liveregex = []
+wikidata = 'Q23022864'
