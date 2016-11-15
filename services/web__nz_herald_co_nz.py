a7efe3890296d46f2e10c15f67f647b1d7ff7dbb
readerboy7 <sstollenwerk@yahoo.co.nz>
2016-11-16 09:36:24 +1300

    adding nzherald to list

---
+++ services/web__nz_herald_co_nz.py
@@ -0,0 +1,12 @@
+refresh = 120
+version = 20161116.01
+
+
+
+urls = ['http://www.nzherald.co.nz']
+
+regex = [r'^https?:\/\/[^\/]*nzherald\.co\.nz'] 
+
+
+videoregex = [r'video\.cfm']
+liveregex = []
