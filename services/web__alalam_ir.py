d465ffa4d42cf2f83bce2fd22bec4a6622ae457d
Arkiver2 <Arkiver@hotmail.com>
2016-11-16 18:30:10 +0100

    Add two Iranian newssites.

---
+++ services/web__alalam_ir.py
@@ -0,0 +1,14 @@
+refresh = 120
+version = 20161116.01
+
+urls = ['http://www.alalam.ir/',
+	'http://en.alalam.ir/',
+	'http://www.alalam.ir/rss/latest',
+	'http://en.alalam.ir/rss',
+	'http://en.alalam.ir/video/latest',
+	'http://en.alalam.ir/albums/others']
+regex = [r'^https?:\/\/[^\/]*alalam\.ir\/']
+videoregex = [r'\/video\/']
+liveregex = []
+
+
---
+++ services/web__presstv_ir.py
@@ -0,0 +1,14 @@
+refresh = 500
+version = 20161116.01
+
+urls = ['http://www.presstv.ir/',
+	'http://www.presstv.ir/Farsi/',
+	'http://www.presstv.ir/French/',
+	'http://www.presstv.ir/RSS/MRSS/1',
+	'http://www.presstv.ir/RSS/MRSS/200']
+regex = [r'^https?:\/\/[^\/]*presstv\.ir\/',
+	r'^https?:\/\/[^\/:]*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+']
+videoregex = []
+liveregex = []
+
+
