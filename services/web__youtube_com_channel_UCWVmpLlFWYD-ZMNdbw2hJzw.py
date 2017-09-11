a108ddd2ba15f6e1d5e4ec5a005f24a032337fe2
Lagittaja <31856524+Lagittaja@users.noreply.github.com>
2017-09-12 01:40:35 +0300

    Add files via upload
    io-tech

---
+++ services/web__youtube_com_channel_UCWVmpLlFWYD-ZMNdbw2hJzw.py
@@ -0,0 +1,7 @@
+refresh = 43200
+version = 20161106.01
+
+urls = ['https://www.youtube.com/channel/UCWVmpLlFWYD-ZMNdbw2hJzw/videos']
+regex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
+videoregex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
+liveregex = []
